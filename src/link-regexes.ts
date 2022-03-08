c5f677a71878ecacab784a4481f72cdde3529779
kuchejak <kuchejak@fit.cvut.cz>
2022-03-08 21:29:17 +0100

    Add support for filenames without spaces

--- src/link-regexes.ts
+++ src/link-regexes.ts
@@ -3,10 +3,10 @@
 */
 
 // Link to one verse, for example "Gen 1.1" or "Gen 1:1"
-export const oneVerseRegEx = new RegExp(/([a-zA-Z0-9]+\s\d+)[,#.:;](\d+)$/);
+export const oneVerseRegEx = new RegExp(/([a-zA-Z0-9]+\s*\d+)[,#.:;](\d+)$/);
 
 // Link to multiple verses, for example "Gen 1,1-5"
-export const multipleVersesRegEx = new RegExp(/([a-zA-Z0-9]+\s\d+)[,#.:;](\d+)\s*[-.=]\s*(\d+)$/);
+export const multipleVersesRegEx = new RegExp(/([a-zA-Z0-9]+\s*\d+)[,#.:;](\d+)\s*[-.=]\s*(\d+)$/);
 
 // Book and chapter string (used for converting to bible study kit file names)
 export const bookAndChapterRegex = /([a-zA-Z0-9 ]+)\s(\d+)/
